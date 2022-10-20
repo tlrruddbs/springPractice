77bf3a39bf2846400cd8ac247fd83773b90401f5
tlrruddbs <tlrruddbs@gmail.com>
2022-10-20 15:48:16 +0900

    jdk 동적 프록시

--- proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -3,6 +3,8 @@ package hello.proxy;
 import hello.proxy.app.config.AppV2Config;
 import hello.proxy.app.config.v1_proxy.ConcreteProxyConfig;
 import hello.proxy.app.config.v1_proxy.InterfaceProxyConfig;
+import hello.proxy.app.config.v2_dynamicproxy.DynamicProxyBasicConfig;
+import hello.proxy.app.config.v2_dynamicproxy.DynamicProxyFilterConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -10,8 +12,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
-@Import(ConcreteProxyConfig.class)
-@SpringBootApplication(scanBasePackages = "hello.proxy.app.config.v1_proxy")
+@Import(DynamicProxyFilterConfig.class)
+@SpringBootApplication(scanBasePackages = "hello.proxy.app.config.v2_dynamicproxy.handler")
 public class ProxyApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProxyApplication.class, args);
