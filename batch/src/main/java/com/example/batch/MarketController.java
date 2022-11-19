c176a1b40d21facea83d47b567f1ab872e818cb7
tlrruddbs <tlrruddbs@gmail.com>
2022-11-19 20:43:00 +0900

    tlrruddbs-6: logback

--- batch/src/main/java/com/example/batch/MarketController.java
+++ batch/src/main/java/com/example/batch/MarketController.java
@@ -16,6 +16,7 @@ public class MarketController {
 
     @GetMapping("ks")
     public String ks() throws Exception{
+        log.trace("log trace");
         log.info("log info!!!");
         log.warn("log warn!!!");
         log.error("log.error");
