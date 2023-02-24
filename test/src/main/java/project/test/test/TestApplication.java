a855364dfc1739c05dab6556a48092d1109a8a3f
kyungsik <tlrruddbs@naver.com>
2023-02-24 23:41:03 +0900

    프로젝트 테스트

--- test/src/main/java/project/test/test/TestApplication.java
+++ test/src/main/java/project/test/test/TestApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TestApplication {
 
 	public static void main(String[] args) {
+		System.out.println("test");
+
 		SpringApplication.run(TestApplication.class, args);
 	}
 
