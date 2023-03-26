569977a74c919520399cb0c14b7d21a97bcb8d21
kyungsik <tlrruddbs@naver.com>
2023-03-27 02:12:18 +0900

    book2: 상품 비지니스 구현

---
+++ bookshop/src/main/java/project/book/shop/service/MemberService.java
@@ -0,0 +1,40 @@
+package project.book.shop.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import project.book.shop.entity.Member;
+import project.book.shop.repository.MemberRepository;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class MemberService {
+    private final MemberRepository memberRepository;
+    /**
+     * 회원가입
+     */
+    @Transactional //변경
+    public Long join(Member member) {
+        validateDuplicateMember(member); //중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByUserName(member.getUserName());
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+    public Member findOne(Long memberId) {
+        return memberRepository.findById(memberId).get();
+    }
+}
