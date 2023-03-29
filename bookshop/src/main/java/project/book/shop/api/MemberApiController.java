2d3373f0f51fb8427e382328d2b9b4fa6931c106
kyungsik <tlrruddbs@naver.com>
2023-03-30 03:17:38 +0900

    book-4: 회원 등록

--- bookshop/src/main/java/project/book/shop/api/MemberApiController.java
+++ bookshop/src/main/java/project/book/shop/api/MemberApiController.java
@@ -2,6 +2,7 @@ package project.book.shop.api;
 
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,14 +17,26 @@ import lombok.Data;
 public class MemberApiController {
     private final MemberService memberService;
 
-    //v1 파라미터는 객체로 받는걸 지양한다.
+    //v1 파라미터는 엔티티로 받는걸 지양한다.
     @PostMapping("/api/v1/members")
     public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member){
         Long id = memberService.join(member);
         return new CreateMemberResponse(id);
     }
     
+    @PostMapping("api/v2/members")
+    public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request){
+        Member member = new Member();
+        member.setUserName(request.getUserName());
 
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    @Data
+    static class CreateMemberRequest{
+        private String userName;
+    }
     @Data
     @AllArgsConstructor
     static class CreateMemberResponse{
