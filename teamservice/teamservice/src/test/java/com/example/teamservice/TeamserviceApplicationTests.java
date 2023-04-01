26f5991d45fe7d608f9f3427365d808b020b2ee9
kyungsik <tlrruddbs@naver.com>
2023-04-02 02:34:37 +0900

    api gateway create team project

---
+++ teamservice/teamservice/src/test/java/com/example/teamservice/TeamserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.teamservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
